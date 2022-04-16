c2bbc80a339d8f6dcf047fb3a9f6a1786e6cde6a
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-04-16 19:43:40 +0600

    uploading work

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form/Form';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center mt-5 text-primary">SMS Sender Application</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
