e377eab1f267c9c2dfdc43647d2862f248ee4d06
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-05-02 00:14:23 +0600

    update firebase config

---
+++ src/firebase/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWaTL-mbAF3f4r6Sx3a6JkfKdW7Xntg-o",
+    authDomain: "react-sms-sender-app.firebaseapp.com",
+    projectId: "react-sms-sender-app",
+    storageBucket: "react-sms-sender-app.appspot.com",
+    messagingSenderId: "466450742027",
+    appId: "1:466450742027:web:c8f6159102b9661063800e"
+};
+
+
+export default firebaseConfig;
