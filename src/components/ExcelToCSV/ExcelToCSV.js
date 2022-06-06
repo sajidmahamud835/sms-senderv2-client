49e037650c1689700e59d29b71af0b8f09d02b14
mr-samsul-alam <contactsamsulalam@gmail.com>
2022-06-06 19:55:03 +0600

    all

--- src/components/ExcelToCSV/ExcelToCSV.js
+++ src/components/ExcelToCSV/ExcelToCSV.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { DataGrid } from "@material-ui/data-grid";
 const ExcelToCSV = () => {
     const [file, setFile] = useState();
     const [array, setArray] = useState([]);
@@ -13,23 +13,24 @@ const ExcelToCSV = () => {
     const csvFileToArray = string => {
         const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
         const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
-
+        console.log(csvRows);
         const array = csvRows.map(i => {
             const values = i.split(",");
+            console.log(values);
             const obj = csvHeader.reduce((object, header, index) => {
                 object[header] = values[index];
+                // I need to add index but here is no option to get add index
+                let x = Math.random();
+                object["id"] = x;
                 return object;
             }, {});
+            console.log(obj);
             return obj;
         });
 
-
-
-
-
         const function4Chacking = (e) => {
-            if (e.number !== '') {
-                console.log('A number is not available');
+            if (e.number === '') {
+                console.log('a number is not available');
             }
             else {
                 setArray(array);
@@ -38,25 +39,34 @@ const ExcelToCSV = () => {
         const chacking = array.map(ar => function4Chacking(ar))
     };
 
-
-
-
-
-
     const handleOnSubmit = (e) => {
         e.preventDefault();
 
         if (file) {
             fileReader.onload = function (event) {
                 const text = event.target.result;
-
                 csvFileToArray(text);
             };
 
             fileReader.readAsText(file);
         }
     };
-
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 100 },
+        {
+            field: 'personName',
+            headerName: 'Name',
+            width: 150,
+            editable: true,
+        },
+        {
+            field: 'number',
+            headerName: 'Number',
+            type: 'number',
+            width: 200,
+            editable: true,
+        },
+    ];
     const headerKeys = Object.keys(Object.assign({}, ...array));
     return (
         <div style={{ textAlign: "center" }} className="newCampaign">
@@ -79,27 +89,16 @@ const ExcelToCSV = () => {
             </form>
 
             <br />
-            
 
-            <table>
-                <thead>
-                    <tr key={"header"}>
-                        {headerKeys.map((key) => (
-                            <th>{key}</th>
-                        ))}
-                    </tr>
-                </thead>
 
-                <tbody>
-                    {array.map((item) => (
-                        <tr key={item.id}>
-                            {Object.values(item).map((val) => (
-                                <td>{val}</td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <DataGrid
+                rows={array}
+                columns={columns}
+                pageSize={20}
+                rowsPerPageOptions={[25]}
+                checkboxSelection
+                disableSelectionOnClick
+            />
         </div>
     );
 };
