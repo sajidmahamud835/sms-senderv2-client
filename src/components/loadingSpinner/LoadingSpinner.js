d53a9bba0772097d04ac3d2fd40219850e958649
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-07-05 14:28:22 +0600

    marge resolved

--- src/components/loadingSpinner/LoadingSpinner.js
+++ src/components/loadingSpinner/LoadingSpinner.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Spinner } from 'react-bootstrap';
-
-const LoadingSpinner = () => {
-    return (
-        <div className='d-flex align-items-center' style={{ height: '90vh' }}>
-            <Spinner animation="border" variant="primary" className='d-block mx-auto' role="status">
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
-        </div>
-    );
-};
-
-export default LoadingSpinner;
-=======
 import { Spinner } from "react-bootstrap";
 import './LoadingSpinner.css';
 const LoadingSpinner = () => {
@@ -35,4 +19,3 @@ const LoadingSpinner = () => {
 };
 
 export default LoadingSpinner;
->>>>>>> dev
