41aa8557f05ee75ad426dfc784e2c0ba18b1d385
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-07-14 13:01:24 +0600

    creating report route

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -1,15 +1,141 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './SMSLogs.css';
+import { useState } from 'react';
+import UseFirebase from '../../Hooks/UseFirebase';
 import PrivateRoute from '../../PrivateRoute/PrivateRoute/PrivateRoute';
-import './Reports.css'
+
+
+//#region Constants
+const userData = {
+    id: "1",
+    name: "John Doe",
+    email: "sajid@gmail.com",
+    phone: "0123456789",
+    address: "Dhaka, Bangladesh",
+    role: "admin",
+    smsTemplates: [
+        {
+            id: "1",
+            message: "Hello, this is a test SMS message.",
+            status: "sent",
+            date: "2020-01-01"
+        },
+        {
+            id: "2",
+            message: "Hello, this is a test SMS message.",
+            status: "sent",
+            date: "2020-01-01"
+        }
+    ],
+    smsLogs: [
+        {
+            id: "1",
+            message: "Hello, this is a test SMS message.",
+            status: "sent",
+            date: "2020-01-01"
+        },
+        {
+            id: "2",
+            message: "Hello, this is a test SMS message.",
+            status: "sent",
+            date: "2020-01-01"
+        }
+    ]
+}
+//#endregion
+
+
+
+const getSMSLogs = (id) => {
+    const url = `${process.env.REACT_APP_SERVER_URL}/sms/logs`;
+    return fetch(url, {
+        method: "GET",
+        headers: {
+            "content-type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            return data;
+        }
+        ).catch((error) => {
+            console.log(error);
+        }
+        );
+}
+
+
 const Reports = () => {
+    const { user, loading } = UseFirebase();
+    const [error, setError] = useState(false);
+    const [smsLogs, setSmsLogs] = useState([]);
+
+    useEffect(() => {
+        getSMSLogs().then((data) => {
+            setSmsLogs(data.messages);
+        }
+        ).catch((error) => {
+            console.log(error);
+        }
+        );
+    }
+        , []);
+
+
     return (
-       
-            <div className='reportsContainer'>
-                <h1>Get Your Reports</h1>
+        <PrivateRoute>
+            <div className="reports">
+                <h1>Reports</h1>
+                <div className="reportsContainer">
+                    <div className="reportsItem">
+                        <h2>SMS Logs</h2>
+                        <p>
+                            This is a list of all SMS messages sent by all user in this server.
+                        </p>
+                        <div className="reportsItemContainer">
+                            <div className="reportsItemContent">
+                                <div className="reportsItemContentHeader">
+                                    <h3>All Messages</h3>
+                                </div>
+                                <div className="reportsItemContentBody">
+                                    <table className="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>To</th>
+                                                <th>From</th>
+                                                <th>SMS Message</th>
+                                                <th>SMS Status</th>
+                                                <th>SMS Date</th>
+                                                <th>Cost</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {loading ? (
+                                                <tr>
+                                                    <td>Loading...</td>
+                                                </tr>
+                                            ) : (
+                                                smsLogs.map((smsLog) => (
+                                                    <tr key={smsLog.sid}>
+                                                        <td>{smsLog.to}</td>
+                                                        <td>{smsLog.from}</td>
+                                                        <td>{smsLog.body}</td>
+                                                        <td><span className='bg-primary text-light rounded p-1 m'>{smsLog.status}</span></td>
+                                                        <td>{smsLog.dateSent}</td>
+                                                        <td>{smsLog.price} {smsLog.priceUnit}</td>
+                                                    </tr>
+                                                ))
+                                            )}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-       
-
+        </PrivateRoute>
     );
-};
-
+}
 export default Reports;
+//#endregion
