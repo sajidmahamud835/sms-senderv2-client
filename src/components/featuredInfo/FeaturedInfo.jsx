7a96a4ba86d6ca779cf2ee4f8edb83e44a38d719
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-07-17 14:30:03 +0600

    updating futuredinfo in home page for admin

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -3,21 +3,33 @@ import "./featuredInfo.css";
 import { Grid } from '@material-ui/core';
 const FeaturedInfo = (props) => {
   const [data, setData] = React.useState(props.data);
+  //use loading to show the loading spinner
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setData(props.data);
+    if (props.data.length > 0) {
+      setLoading(false);
+    }
+  }
+    , [props.data]);
   return (
     <div className="featured">
-      <Grid container spacing={1}>
-        {data.map((item) =>
-          <Grid sm={12} md={6} lg={4} key={item.id} style={{ width: "100%" }}>
-            <div className="featuredItem">
-              <span className="featuredTitle">{item.name}</span>
-              <div className="featuredMoneyContainer">
-                {item.icon}
-                <span className="featuredMoney">{item.value}</span>
-              </div>
-            </div>
+      {
+        loading ? <div className="loading">Loading...</div> :
+          <Grid container spacing={1}>
+            {data.map((item) =>
+              <Grid item sm={12} md={6} lg={4} key={item.id} style={{ width: "100%" }}>
+                <div className="featuredItem">
+                  <span className="featuredTitle">{item.icon} {item.name}</span>
+                  <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">{item.value}</span>
+                  </div>
+                </div>
+              </Grid>
+            )}
           </Grid>
-        )}
-      </Grid>
+      }
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
@@ -8,8 +8,61 @@ import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import { Grid } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [campaignsCount, setCampaignsCount] = useState({});
+  const [usersCount, setUsersCount] = useState({});
+  const [activeCampaigns, setActiveCampaigns] = useState(0);
+  const [activeUsers, setActiveUsers] = useState(0);
+  const [pendingCampaigns, setPendingCampaigns] = useState(0);
+  const [pendingUsers, setPendingUsers] = useState(0);
+  const [totalCampaigns, setTotalCampaigns] = useState(0);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setActiveCampaigns(campaignsCount.activeCampaigns);
+    setActiveUsers(usersCount.activeUsersCount);
+    setPendingCampaigns(campaignsCount.scheduledCampaigns);
+    setPendingUsers(usersCount.inactiveUsersCount);
+    setTotalCampaigns(campaignsCount.activeCampaigns + campaignsCount.scheduledCampaigns);
+    setTotalUsers(usersCount.activeUsersCount + usersCount.inactiveUsersCount);
+    console.log(campaignsCount, usersCount);
+  }
+    , [campaignsCount, usersCount]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_SERVER_URL}/campaigns/count/`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+      }
+    })
+      .then((res) => {
+        if (res.status === 403 || res.status === 401) {
+          navigate('/login');
+        } else {
+          return res.json();
+        }
+      }
+      )
+      .then((data) => setCampaignsCount(data));
+
+    fetch(`${process.env.REACT_APP_SERVER_URL}/users/count/`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+      }
+    })
+      .then((res) => {
+        if (res.status === 403 || res.status === 401) {
+          navigate('/login');
+        } else {
+          return res.json();
+        }
+      }
+      )
+      .then((data) => setUsersCount(data));
+  }, [navigate]);
 
   return (
     <div className="home">
@@ -17,22 +70,41 @@ const Home = () => {
         {
           id: 1,
           name: "Active User",
-          value: "10",
+          value: activeUsers,
           icon: <FiUserCheck />,
         },
         {
           id: 2,
           name: "Pending User",
-          value: "10",
+          value: pendingUsers,
           icon: <FiUser />,
         },
         {
           id: 3,
+          name: "Total User",
+          value: totalUsers,
+          icon: <FiUser />,
+        },
+        {
+          id: 4,
           name: "Active Campaign",
-          value: "10",
+          value: activeCampaigns,
           icon: <Message />,
         },
+        {
+          id: 5,
+          name: "Pending Campaign",
+          value: pendingCampaigns,
+          icon: <Message />,
+        },
+        {
+          id: 6,
+          name: "Total Campaign",
+          value: totalCampaigns,
+          icon: <Message />,
+        }
       ]} />
+
       <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
       <div className="homeWidgets">
         <Grid container spacing={1}>
