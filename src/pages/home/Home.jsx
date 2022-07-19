fee1ff99e5953b9af8cdb424eb3573edbfc20074
shadik <shadikmahamudsohan@gmail.com>
2022-07-20 00:03:53 +0600

    showing campaign data for user

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,9 +8,10 @@ import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import { Grid } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
-
+import UseFirebase from '../../Hooks/UseFirebase';
 const Home = () => {
   const [campaignsCount, setCampaignsCount] = useState({});
+  const [userCampaignsCount, setUserCampaignsCount] = useState({});
   const [usersCount, setUsersCount] = useState({});
   const [activeCampaigns, setActiveCampaigns] = useState(0);
   const [activeUsers, setActiveUsers] = useState(0);
@@ -20,49 +21,71 @@ const Home = () => {
   const [totalUsers, setTotalUsers] = useState(0);
   const [smsLogs, setSmsLogs] = useState([]);
   const navigate = useNavigate();
+  const { admin, isAdminLoading, user } = UseFirebase();
 
   useEffect(() => {
-    setActiveCampaigns(campaignsCount.activeCampaigns);
-    setActiveUsers(usersCount.activeUsersCount);
-    setPendingCampaigns(campaignsCount.scheduledCampaigns);
-    setPendingUsers(usersCount.inactiveUsersCount);
-    setTotalCampaigns(campaignsCount.activeCampaigns + campaignsCount.scheduledCampaigns);
-    setTotalUsers(usersCount.activeUsersCount + usersCount.inactiveUsersCount);
-    console.log(campaignsCount, usersCount);
+    if (!isAdminLoading && admin) {
+      setActiveCampaigns(campaignsCount.activeCampaigns);
+      setActiveUsers(usersCount.activeUsersCount);
+      setPendingCampaigns(campaignsCount.scheduledCampaigns);
+      setPendingUsers(usersCount.inactiveUsersCount);
+      setTotalCampaigns(campaignsCount.activeCampaigns + campaignsCount.scheduledCampaigns);
+      setTotalUsers(usersCount.activeUsersCount + usersCount.inactiveUsersCount);
+      console.log(campaignsCount, usersCount);
+    }
   }
     , [campaignsCount, usersCount]);
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_SERVER_URL}/campaigns/count/`, {
-      headers: {
-        authorization: `Bearer ${localStorage.getItem('accessToken')}`
-      }
-    })
-      .then((res) => {
-        if (res.status === 403 || res.status === 401) {
-          navigate('/login');
-        } else {
-          return res.json();
-        }
-      }
-      )
-      .then((data) => setCampaignsCount(data));
+    if (!isAdminLoading) {
+      if (admin) {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/campaigns/count/`, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+          }
+        })
+          .then((res) => {
+            if (res.status === 403 || res.status === 401) {
+              navigate('/login');
+            } else {
+              return res.json();
+            }
+          }
+          )
+          .then((data) => setCampaignsCount(data));
 
-    fetch(`${process.env.REACT_APP_SERVER_URL}/users/count/`, {
-      headers: {
-        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        fetch(`${process.env.REACT_APP_SERVER_URL}/users/count/`, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+          }
+        })
+          .then((res) => {
+            if (res.status === 403 || res.status === 401) {
+              navigate('/login');
+            } else {
+              return res.json();
+            }
+          }
+          )
+          .then((data) => setUsersCount(data));
+      } if (!admin) {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/campaigns/count/${user?.email}`, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+          }
+        })
+          .then((res) => {
+            if (res.status === 403 || res.status === 401) {
+              navigate('/login');
+            } else {
+              return res.json();
+            }
+          }
+          )
+          .then((data) => setUserCampaignsCount(data));
       }
-    })
-      .then((res) => {
-        if (res.status === 403 || res.status === 401) {
-          navigate('/login');
-        } else {
-          return res.json();
-        }
-      }
-      )
-      .then((data) => setUsersCount(data));
-  }, [navigate]);
+    }
+  }, [admin, isAdminLoading, navigate, user.email]);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_SERVER_URL}/sms/logs/month`, {
@@ -84,59 +107,103 @@ const Home = () => {
       );
   }, [navigate]);
 
+  useEffect(() => {
+    if (!admin && !isAdminLoading) {
+      console.log(userCampaignsCount);
+      setActiveCampaigns(userCampaignsCount.active);
+      setPendingCampaigns(userCampaignsCount.scheduled);
+      setTotalCampaigns(userCampaignsCount.active + userCampaignsCount.scheduled);
+    }
+  }, [admin, isAdminLoading, userCampaignsCount]);
+
 
   return (
     <div className="home">
-      <FeaturedInfo data={[
-        {
-          id: 1,
-          name: "Active Users",
-          value: activeUsers,
-          icon: <FiUserCheck />,
-        },
-        {
-          id: 2,
-          name: "Pending Users",
-          value: pendingUsers,
-          icon: <FiUser />,
-        },
-        {
-          id: 3,
-          name: "Total Users",
-          value: totalUsers,
-          icon: <FiUser />,
-        },
-        {
-          id: 4,
-          name: "Active Campaigns",
-          value: activeCampaigns,
-          icon: <Message />,
-        },
-        {
-          id: 5,
-          name: "Pending Campaigns",
-          value: pendingCampaigns,
-          icon: <Message />,
-        },
-        {
-          id: 6,
-          name: "Total Campaigns",
-          value: totalCampaigns,
-          icon: <Message />,
-        }
-      ]} />
+      {admin &&
+        <FeaturedInfo data={[
+          {
+            id: 1,
+            name: "Active Users",
+            value: activeUsers,
+            icon: <FiUserCheck />,
+          },
+          {
+            id: 2,
+            name: "Pending Users",
+            value: pendingUsers,
+            icon: <FiUser />,
+          },
+          {
+            id: 3,
+            name: "Total Users",
+            value: totalUsers,
+            icon: <FiUser />,
+          },
+          {
+            id: 4,
+            name: "Active Campaigns",
+            value: activeCampaigns,
+            icon: <Message />,
+          },
+          {
+            id: 5,
+            name: "Pending Campaigns",
+            value: pendingCampaigns,
+            icon: <Message />,
+          },
+          {
+            id: 6,
+            name: "Total Campaigns",
+            value: totalCampaigns,
+            icon: <Message />,
+          }
+        ]} />}
 
-      {smsLogs.length > 0 ? <Chart data={smsLogs} title="Monthly API Analytics" grid dataKey="SMS Sent" /> : <div className='noData'>Loading Monthly API Analytics...</div>}
+      {!admin &&
+        <FeaturedInfo data={[
+          {
+            id: 1,
+            name: "Active Campaigns",
+            value: activeCampaigns,
+            icon: <Message />,
+          },
+          {
+            id: 2,
+            name: "Pending Campaigns",
+            value: pendingCampaigns,
+            icon: <Message />,
+          },
+          {
+            id: 3,
+            name: "Total Campaigns",
+            value: totalCampaigns,
+            icon: <Message />,
+          }
+        ]}
+        />}
+
+      {admin &&
+        <div>
+          {smsLogs.length > 0 ? <Chart data={smsLogs} title="Monthly API Analytics" grid dataKey="SMS Sent" /> : <div className='noData'>Loading Monthly API Analytics...</div>}
+        </div>
+      }
+
+
       <div className="homeWidgets">
         <Grid container spacing={1}>
-          <Grid item style={{ width: "100%" }} sm={12} md={6}>
-            <WidgetSm />
-          </Grid>
+          {admin &&
+            <Grid item style={{ width: "100%" }} sm={12} md={6}>
+              <WidgetSm />
+            </Grid>
+          }
           <Grid item style={{ width: "100%" }} sm={12} md={6}>
             <WidgetLg />
           </Grid>
+
         </Grid>
       </div>
+
+
     </div>
   );
 };
